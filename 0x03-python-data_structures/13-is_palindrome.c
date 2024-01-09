6832b71c82ede2e963f469f3211a788201b30581
Wisdom <mawiztechnology@gmail>
2024-01-08 16:37:02 -0800

    task 3

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ *reverse_listint - reverses a linked list
+ *@head: pointer to the first node in the list
+ *
+ *Return: pointer to the first node in the new list
+ */
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+its dat structure time
