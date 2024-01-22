df3985fc443ca6e739eaeca01a26ec2149e010cb
Wisdom <mawiztechnology@gmail>
2024-01-22 11:40:59 -0800

    its time

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+
+void print_python_list(PyObject *p)
+void print_python_bytes(PyObject *p)
+void print_python_float(PyObject *p)
+
+
+int main():
+	void print_python_list(PyObject *p)
+	void print_python_bytes(PyObject *p)
+	void print_python_float(PyObject *p)
+
+ void print_python_list(PyObject *p):
+	 PyObject *p;
+	 PyObject *p;
+
+	 if p =!PyObject:
+	 exception:
+	 printf("p is not valid")
